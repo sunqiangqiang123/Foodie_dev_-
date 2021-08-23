2c743f256e7376472ef38714d0c7f438e921f3f8
孙伟 <@qq.com>
2021-08-23 18:01:36 +0800

    SWAGGER2

--- foodie-service/src/main/java/com/sun/service/UserService.java
+++ foodie-service/src/main/java/com/sun/service/UserService.java
@@ -1,5 +1,8 @@
 package com.sun.service;
 
+import com.sun.pojo.BO.UserBO;
+import com.sun.pojo.model.SysUsers;
+
 public interface UserService {
 
 
@@ -7,4 +10,11 @@ public interface UserService {
      * 判断用户是否存在
      */
     public boolean queryUserNameIsExist(String username);
+
+    /**
+     * 创建用户
+     * @param userBO
+     * @return
+     */
+    public SysUsers createUser(UserBO userBO);
 }
--- foodie-service/src/main/java/com/sun/service/impl/UserServiceImpl.java
+++ foodie-service/src/main/java/com/sun/service/impl/UserServiceImpl.java
@@ -1,19 +1,25 @@
 package com.sun.service.impl;
 
 import com.sun.mapper.SysUsersMapper;
+import com.sun.pojo.BO.UserBO;
 import com.sun.pojo.model.SysUsers;
 import com.sun.service.UserService;
+import com.sun.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
+import java.security.NoSuchAlgorithmException;
+
 @Service
 public class UserServiceImpl implements UserService {
 
     @Autowired
     public SysUsersMapper sysUsersMapper;
+
+    private static final String USER_FACE = "ddd";
 
     @Transactional(propagation = Propagation.SUPPORTS)
     @Override
@@ -25,5 +31,22 @@ public class UserServiceImpl implements UserService {
         SysUsers result = sysUsersMapper.selectOneByExample(userExample);
 
         return result == null ? false:true;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public SysUsers createUser(UserBO userBO) {
+
+        try {
+            SysUsers sysUsers = new SysUsers();
+            sysUsers.setUsername(userBO.getUsername());
+            sysUsers.setPassword(MD5Utils.getMD5Str(userBO.getPassword()));
+            sysUsers.setNickname(userBO.getUsername());//昵称默认用户名称
+            sysUsers.setFace(USER_FACE);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
